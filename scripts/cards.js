2a7b3d720198d9cfaf7fc77e2d5e35b3aa6fbd1a
Maria Sozdateleva <macreatoor@yandex.ru>
2024-06-03 11:43:36 +0300

    1

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,23 +1,23 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Это Бебетка",
+      link: "./images/bebetka_1.jpeg",
     },
     {
       name: "Челябинская область",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Крейзи Беба",
+      link: "./images/bebetka_2.jpeg",
     },
     {
       name: "Камчатка",
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Сонный Бебстер",
+      link: "./images/bebetka_3.jpeg",
     },
     {
       name: "Байкал",
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
-// @todo: Темплейт карточки
 
-// @todo: DOM узлы
+// Функция создания карточки
+function createCard(cardData, deleteCallback) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
 
-// @todo: Функция создания карточки
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
+    cardElement.querySelector('.card__image').alt = cardData.name;
 
-// @todo: Функция удаления карточки
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCallback);
 
-// @todo: Вывести карточки на страницу
+    return cardElement;
+}
+// Функция удаления карточки
+function deleteCard(event) {
+    const card = event.target.closest('.card');
+    card.remove();
+}
+
+// Вывод карточки на страницу
+
+function outCards(cards) {
+    const cardList = document.querySelector('.places__list');
+    cards.forEach((cardData) => {
+        const cardElement = createCard(cardData, deleteCard);
+        cardList.append(cardElement);
+    });
+}
+
+outCards(initialCards);
